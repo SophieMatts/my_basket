7e3c16b3e1a359dfe32b9b9f680aa4fc969b9d2b
sophie mattacks <s.mattacks@hotmail.co.uk>
2022-07-02 18:53:16 +0100

    Add function complete

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,15 +1,27 @@
 require "google/cloud/datastore"
 
 class ListsController < ApplicationController
+  $datastore = Google::Cloud::Datastore.new(
+    project_id: 'plated-mesh-355110',
+    credentials: '/Users/sophiemattacks/Repos/Google/my_basket/plated-mesh-355110-58e596b5549b.json'
+  )
+
 
   def index
-    datastore = Google::Cloud::Datastore.new(
-      project_id: 'plated-mesh-355110',
-      credentials: '/Users/sophiemattacks/Repos/Google/my_basket/plated-mesh-355110-58e596b5549b.json'
-    )
-
-    query = datastore.query('Item')
+    query = $datastore.query('Item')
       # where ("done", "=", false)
-    @items = datastore.run query
+    @items = $datastore.run query
+  end
+
+  def add_item
+    item = $datastore.entity 'Item' do |t|
+      t['name'] = params[:item_name]
+      t['email'] = 'sophiemattacks1@gmail.com'
+      t['quantity'] = '1'
+    end
+
+    $datastore.save item
+
+    redirect_to '/lists/index'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   get 'lists/index'
   get 'auth/login'
   get '/auth/google_oauth2/callback', to: 'auth#handle_auth'
-  root "auth#login"
+
+  post '/add_item' , to: 'lists#add_item'
+  root 'auth#login'
 end
