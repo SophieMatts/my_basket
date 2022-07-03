db59135bcc64d28d15c04ac574f789a094368d98
sophie mattacks <s.mattacks@hotmail.co.uk>
2022-07-03 12:40:27 +0100

    Add email to session

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,9 +1,10 @@
 class AuthController < ApplicationController
   def login
+    redirect_to '/lists/index' unless session[:email].nil?
   end
 
   def handle_auth
-    puts request.env['omniauth.auth'].info.email
+    session[:email] = request.env['omniauth.auth'].info.email
     redirect_to '/lists/index'
   end
 end
